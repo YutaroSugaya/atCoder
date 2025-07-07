a5b81bc77dd09545b0bdb1da96e0e841f5982f51
Yutaro.Sugaya <tech_nine0400@yahoo.co.jp>
2025-07-08 07:01:18 +0900

    b

--- abc413/b/main.py
+++ abc413/b/main.py
@@ -1,10 +1,17 @@
-import math
-from collections import defaultdict
-
+import itertools
 
 def main():
-    # Your solution here
-    pass
+    n = int(input())    
+    strings = [input().strip() for _ in range(n)]
+    car = []
+    ans = 0
+    for perm in itertools.permutations(range(n), 2):
+        i, j = perm
+        result = strings[i] + strings[j]
+        if result not in car:
+            ans += 1
+            car.append(result)
+    print(ans)
 
 if __name__ == "__main__":
     main()
